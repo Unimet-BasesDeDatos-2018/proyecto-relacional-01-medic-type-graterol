e9c995840e0f65993d4278653cdfae61031a0642
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2017-11-09 19:30:59 -0400

    Commit inicial

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const path = require('path');
+
+
+// Iniciar la applicacion:
+module.exports.app = express();
+const app = module.exports.app;
+
+const port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// Middleware:
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs');
+});
+
+
+// Iniciar el servidor:
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
