8c2fbd596500ce981ab7a65734c84274d0b1d81d
ManuOjeda13 <ojedamanuel@correo.unimet.edu.ve>
2017-11-15 16:12:07 -0430

    Pasar info de pacient a vista cuentaPaciente.hbs

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,37 @@
 const server = require('./../server');
+const Persona = require('./../models/modelo-persona');
+const Paciente = require('./../models/modelo-paciente');
+const Doctor = require('./../models/modelo-doctor');
 const app = server.app;
 
 app.post('/cuenta-paciente', (req, res) => {
-  res.render('cuentaPaciente.hbs');
+
+  let cedulaIngresada = parseInt(req.body.cedula);
+
+  Persona.findOne({where: { Cedula: cedulaIngresada }})
+  .then((persona) => {
+    let idPersona = persona.dataValues.idPersona;
+    let miPersona = persona.dataValues;
+
+    Paciente.findOne({where: { idPersona: idPersona }})
+    .then((paciente) => {
+
+      miPersona.LugarNacimiento = paciente.dataValues.LugarNacimiento;
+      miPersona.TipoSangre = paciente.dataValues.TipoSangre;
+      miPersona.ReferidoPor = paciente.dataValues.ReferidoPor;
+
+      console.log(miPersona);
+
+      res.render('cuentaPaciente.hbs', {miPersona});
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 });
 
 app.post('/cuenta-doctor', (req, res) => {
