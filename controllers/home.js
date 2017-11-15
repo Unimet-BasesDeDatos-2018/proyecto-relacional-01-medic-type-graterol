fcebca4ca478a82a927d1f7fde2963e039145b49
ManuOjeda13 <ojedamanuel@correo.unimet.edu.ve>
2017-11-15 15:20:41 -0430

    Agregar insertar.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,4 @@
 const server = require('./../server');
-const Persona = require('./../models/modelo-persona');
-
 const app = server.app;
 
 app.post('/cuenta-paciente', (req, res) => {
@@ -14,14 +12,3 @@ app.post('/cuenta-doctor', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render('signup.hbs');
 });
-
-// Persona.create({
-//   idPersona: 1,
-//   Cedula: 24556891,
-//   Nombre: 'Pedro',
-//   Snombre: 'Ignacio',
-//   Apellido: 'Paramo',
-//   Sexo: 'Masculino',
-//   FechaNacimiento: new Date(1990, 0, 12, 0, 0, 0, 0), // new Date(year, month, date, hours, minutes, seconds, milliseconds);
-//   Activo: true
-// });
--- controllers/orm-connection.js
+++ controllers/orm-connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('MedicDatabase', 'root', 'password', {
+const connection = new Sequelize('MedicDatabase', 'root', '21291164', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false
