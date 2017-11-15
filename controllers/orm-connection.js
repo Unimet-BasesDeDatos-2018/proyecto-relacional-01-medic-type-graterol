50aa8334feb97ef1b769821fed6ed2dc1f19099d
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2017-11-15 17:31:37 -0400

    Cambiar clave de coneccion cn orm

--- controllers/orm-connection.js
+++ controllers/orm-connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('MedicDatabase', 'root', '21291164', {
+const connection = new Sequelize('MedicDatabase', 'root', 'password', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false
