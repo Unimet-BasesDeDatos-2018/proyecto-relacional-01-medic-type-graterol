c5d6794cc6211b020e318dd50e58ecc6b2729b54
ManuOjeda13 <ojedamanuel@correo.unimet.edu.ve>
2017-11-16 00:33:41 -0430

    Mostrar en ver informe toda la informacion.

--- controllers/verInforme.js
+++ controllers/verInforme.js
@@ -5,18 +5,52 @@ const Doctor = require('./../models/modelo-doctor');
 const Informes = require('./../models/modelo-informe');
 const app = server.app;
 
-app.post('/informe/:id', (req, res) => {
+app.post('/informe/:id', (req, res) =>
+{
   let idInforme = req.params.id;
 
   Informes.findOne({
     where: {
       idInforme: idInforme
     }
-  }).then((informe) => {
-    let miInforme = informe.dataValues;
+  }).then(informe =>
+    {
+      let miInforme = informe.dataValues;
 
-    // code goes here
+      Persona.findOne({
+        where: {
+          idPersona: informe.dataValues.idDoctor
+        }
+      }).then(persona =>
+        {
+          miInforme.nombreDoctor = `${persona.dataValues.Nombre} ${persona.dataValues.Apellido}`;
 
-    res.render('verInforme.hbs', { miInforme });
-  });
+          Persona.findOne({
+            where: {
+              idPersona : informe.dataValues.idPaciente
+            }
+          }).then(persona =>
+            {
+              miInforme.nombrePaciente = `${persona.dataValues.Nombre} ${persona.dataValues.Apellido}`;
+              miInforme.sexoPaciente = persona.dataValues.Sexo;
+              miInforme.fechaPaciente = persona.dataValues.FechaNacimiento;
+              miInforme.cedulaPaciente = persona.dataValues.Cedula;
+
+              Paciente.findOne({
+                where: {
+                  idPersona : informe.dataValues.idPaciente
+                }
+              }).then(paciente => {
+
+                miInforme.sangrePaciente = paciente.dataValues.TipoSangre;
+
+
+                console.log(miInforme);
+
+                res.render('verInforme.hbs', {miInforme});
+
+              });
+            });
+        });
+    });
 });
