de8fdc73abc90f2702a902450051f0d9cd77e209
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2017-11-15 23:28:58 -0400

    Formato para pasar informacion a verInforme.hbs

--- controllers/verInforme.js
+++ controllers/verInforme.js
@@ -6,5 +6,17 @@ const Informes = require('./../models/modelo-informe');
 const app = server.app;
 
 app.post('/informe/:id', (req, res) => {
-  res.render('verInforme.hbs');
+  let idInforme = req.params.id;
+
+  Informes.findOne({
+    where: {
+      idInforme: idInforme
+    }
+  }).then((informe) => {
+    let miInforme = informe.dataValues;
+
+    // code goes here
+
+    res.render('verInforme.hbs', { miInforme });
+  });
 });
